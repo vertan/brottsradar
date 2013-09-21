04963944cf1d147f5f382f54032f6c9167b469b6
Jesper Bränn <brann.jesper@gmail.com>
2013-09-21 10:30:44 +0200

    Add Brott-model

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class Brott(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    place = db.Column(db.String)
+    date = db.Column(db.DateTime, index = True)
+    longitude = db.Column(db.Float, index = True)
+    latitude = db.Column(db.Float, index = True)
+
