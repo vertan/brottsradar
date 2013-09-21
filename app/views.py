123bc8968d6cc087b4c21c92f61787b77bdd1261
Filip Hedman <hedman.filip@gmail.com>
2013-09-21 11:52:26 +0200

    Fixed unicode string

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    title = 'Är det säkert att gå ut?'
+    title = u"Är det säkert att gå ut?"
     answer = 'hide yo wife, hide yo kids'
     return render_template('index.html',
                            title = title,
